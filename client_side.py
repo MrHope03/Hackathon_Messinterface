476476ab914580079cc20ab45961a177bdde2a4d
Saikumarran SK <73155284+sh4ded@users.noreply.github.com>
2021-03-26 13:03:34 +0530

    Update client_side.py

--- client_side.py
+++ client_side.py
@@ -4,8 +4,79 @@ import socket
 import tqdm
 import os
 
+import datetime
+import calendar
+
+import csv
+
+x = datetime.datetime.now()
+day = x.strftime("%a")
+time = int(x.strftime("%H"))
+
+if (time>6 and time <12):
+    greet = "Morning"
+    inf = "Afternoon"
+    fday = day
+elif (time>=12 and time<18):
+    greet = "Afternoon"
+    inf = "Night"
+    fday = day
+elif (time>=18 and time <21):
+    greet = "Evening"
+    inf = "Night"
+    fday = day
+else:
+    greet = "Night"
+    inf = "Tomorrow"
+    fday = (x + datetime.timedelta(days=1)).strftime("%A")
+
+print("Good " + greet + "! Here's " + inf + "'s menu\n")
+
+food = []
+
+with open(fday + '_' + inf + '.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            food += [[row[0], row[1]]]
+            line_count += 1
+
+for i in food:
+    print(i[0] + '\n')
+
+fields = ['Food', 'Category', 'Appetite']
+
+data = []
+
+foo = sorted(food, key = lambda x: x[1])
+print(foo)
+
+for i in foo:
+    if i[1]=='1':
+        print("How much quanity of " + i[0] + " do you feel you can eat? (on a scale of 0 to 5)\n")
+        data += [[i[0], i[1], input()]]
+    elif i[1]=='2':
+        print("How many cups of " + i[0] + " do you feel you can drink?")
+        data += [[i[0], i[1], input()]]
+    elif i[1]=='3':
+        print("How many nos. of " + i[0] + " do you feel like eating?")
+        data += [[i[0], i[1], input()]]
+    elif i[1]=='4':
+        print("Do you feel like having " + i[0] + " along with the main dish?")
+        data += [[i[0], i[1], input()]]
+        
+filen = "appetite_data.txt"
+
+with open(filen, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(data)
+
 SEPARATOR = "<SEPARATOR>"
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 4096 
 
 host = "192.168.1.101"
 
