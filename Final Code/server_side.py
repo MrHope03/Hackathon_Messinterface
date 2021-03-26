d41bd4e6a0f2e930cb97039acf8a7cd7f8d576e1
Saikumarran SK <73155284+sh4ded@users.noreply.github.com>
2021-03-26 16:50:33 +0530

    Add files via upload

---
+++ Final Code/server_side.py
@@ -0,0 +1,36 @@
+import socket
+import tqdm
+import os
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5001
+
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+s = socket.socket()
+
+s.bind((SERVER_HOST, SERVER_PORT))
+
+print("Enter no. of students : ")
+n = int(input())
+
+for i in range(n):
+    s.listen(n)
+    client_socket, address = s.accept()
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "ab") as f:
+        while True:
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+
+client_socket.close()
+
+s.close()
